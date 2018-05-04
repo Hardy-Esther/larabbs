e7d41a6f38b23dbb8dad1dcbcba116a674fcf7eb
Learner <1176566869@qq.com>
2018-05-04 21:28:07 +0800

    Finish basic page layout

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,3 +5,10 @@
  * Date: 2018/5/4
  * Time: 18:59
  */
+
+if (!function_exists('route_class')) {
+    function route_class()
+    {
+        return str_replace('.', '-', Route::currentRouteName());
+    }
+}
