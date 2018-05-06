39c6426d79f0777f75ce956503410bb2533d00c0
Learner <1176566869@qq.com>
2018-05-06 11:48:04 +0800

    User can create topic

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -12,3 +12,17 @@ if (!function_exists('route_class')) {
         return str_replace('.', '-', Route::currentRouteName());
     }
 }
+
+if (!function_exists('make_excerpt')) {
+    /**
+     * @param $value
+     * @param int $length
+     * @return string
+     * 文章摘要
+     */
+    function make_excerpt($value, $length = 200)
+    {
+        $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+        return str_limit($excerpt, $length);
+    }
+}
